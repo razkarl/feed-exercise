28a227d3de25f2944cceab064edfd614149889cb
Raz <rkarl@lightricks.com>
2022-04-26 18:00:02 +0300

    Use Retrofit, Implement a UnitTest (Fails, needs to use Moshi)...

--- app/src/main/java/com/lightricks/feedexercise/data/FeedRepository.kt
+++ app/src/main/java/com/lightricks/feedexercise/data/FeedRepository.kt
@@ -1,9 +1,25 @@
 package com.lightricks.feedexercise.data
 
+import com.lightricks.feedexercise.network.FeedApiService
+import com.lightricks.feedexercise.network.GetFeedResponse
+import io.reactivex.Single
+import retrofit2.Call
+import retrofit2.Retrofit
+
 /**
  * This is our data layer abstraction. Users of this class don't need to know
  * where the data actually comes from (network, database or somewhere else).
  */
 class FeedRepository {
-    //todo: implement
+    private lateinit var retrofit: Retrofit
+    private lateinit var service: FeedApiService
+
+    constructor() {
+        retrofit = Retrofit.Builder().baseUrl(FeedApiService.baseURL).build()
+        service = retrofit.create(FeedApiService::class.java)
+    }
+
+    fun getRequest(request: String): Call<Single<GetFeedResponse>> {
+        return service.getRequest(request)
+    }
 }
--- app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
@@ -1,5 +1,25 @@
 package com.lightricks.feedexercise.network
 
+import io.reactivex.Single
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 /**
  * todo: add the FeedApiService interface and the Retrofit and Moshi code here
  */
+interface FeedApiService {
+    companion object {
+        val baseURL: String = "http://assets.swishvideoapp.com/Android/demo/catalog/thumbnails/"
+    }
+
+    // Execute a GET request and return an RxJava’s Single that contains the response.
+    @GET("{uri}")
+    fun getRequest(@Path("uri") uri: String): Call<Single<GetFeedResponse>>
+}
+
+class GetFeedResponse {
+    // RKARL: TODO - Implement with Moshi
+    // Use Moshi to de-JSONify things
+}
