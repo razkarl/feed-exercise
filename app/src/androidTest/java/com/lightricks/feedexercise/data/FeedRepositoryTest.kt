19e1789b3b1c97a942186b1611b43899951dabe1
Raz <rkarl@lightricks.com>
2022-04-28 10:51:10 +0300

    Remove a test implemented in the the wrong place

--- app/src/androidTest/java/com/lightricks/feedexercise/data/FeedRepositoryTest.kt
+++ app/src/androidTest/java/com/lightricks/feedexercise/data/FeedRepositoryTest.kt
@@ -1,21 +1,12 @@
 package com.lightricks.feedexercise.data
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Test
 import org.junit.runner.RunWith
 
 // for assertions on Java 8 types (Streams and java.util.Optional)
 import com.google.common.truth.Truth.assertThat
-import com.google.common.truth.Truth.assertWithMessage
 
 @RunWith(AndroidJUnit4::class)
 class FeedRepositoryTest {
    //todo: add the tests here
-
-    @Test
-    fun FeedRepository_ValidGetRequest_ReturnsValidResponse(){
-        val feedApiService = FeedRepository()
-        val response = feedApiService.getRequest("BikeThumbnail.jpg")
-        assertThat(response)
-    }
 }
